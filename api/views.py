3428102f2b23b04c670688059d23e60f44d8f0d4
Usef <sirealkillerjack@gmail.com>
2018-04-03 21:18:41 +0100

    fix requests and some ui stuff

--- api/views.py
+++ api/views.py
@@ -91,58 +91,28 @@ class Api(View):
         return JsonResponse(response)
 
 
+def base(request):
+    base = request.GET.get('base')
+    data = get_data()
+    soup = BeautifulSoup(data, 'html.parser')
+    time = soup.find(time=re.compile(".*"))
+    currency_base = soup.find(currency=base)
+    rate_base = currency_base['rate']
+    currencies = soup.findAll(currency=re.compile(".*"))
+    resp = {}
+    resp['base'] = base
+    resp['time'] = time
+    for currency in currencies:
+        resp[currency['currency']] = float(currency['rate'])/float(rate_base)
+
+    return JsonResponse(resp)
+
 class Latest(View):
 
     def __init__(self):
-        pass
+        self.data = get_data()
 
-    def get_data(self):
-        shelf = shelve.open('exchange_rates')
-        if 'Last-Modified' in shelf:
-            print("################################################")
-            print("Data exist in cache, but checking for deprecation!")
-            headers = {'If-Modified-Since': shelf['Last-Modified']}
-            response = requests.get(url_daily, headers=headers)
-            try:
-                response.raise_for_status()
-            except:
-                print("Problem fetching data from network!!!")
-
-            if response.text == '':
-                print("Data still good")
-                file = open("exchange_rates.xml")
-                self.data = file.read()
-                file.close()
-            else:
-                print("Data is outdated")
-                self.data = response.text
-                shelf['Last-Modified'] = response.headers['Last-Modified']
-                file = open("exchange_rates.xml", "w")
-                file.write(self.data)
-                file.close()
-        else:
-            print("################################################")
-            print("Fetching data from network")
-            response = requests.get(url_daily)
-            try:
-                response.raise_for_status()
-            except:
-                print("Problem fetching data from network!!!")
-
-            html = response.text
-            shelf['Last-Modified'] = response.headers['Last-Modified']
-            self.data = html
-            file = open("exchange_rates.xml", "w")
-            file.write(html)
-            file.close()
-
-    def symbols(self):
-        pass
-
-    def base(self):
-        pass
-
-    def parseData(self, symbols=None):
+    def parseData(self, base=None, symbols=None):
         rates = {}
         resp = {}
         soup = BeautifulSoup(self.data, 'html.parser')
@@ -155,18 +125,27 @@ class Latest(View):
                 print("target {} of type {}.".format(target, type(target)))
 
             lst = [soup.find(currency=re.compile(target))
-                             for target in targets]
+                             for target in targets if target.upper()!='EUR']
             print("###########################################")
             print("list: %s" % lst)
         else:
             lst = soup.findAll(currency=re.compile(".*"))
             print("###########################################")
             print("list: %s" % lst)
+        
+        if base and base.upper() != 'EUR':
+            resp['base'] = base
+            currency_base = soup.find(currency=base)
+            rate_base = currency_base['rate']
+            if 'EUR' in targets:
+                rates['EUR'] = round(1/float(rate_base), 4)
+            for item in lst:
+                rates[item['currency']] = round(float(item['rate'])/float(rate_base), 4)
+        else:
+            resp['base'] = "EUR"
+            for item in lst:
+                rates[item['currency']] = float(item['rate'])            
 
-        for item in lst:
-            rates[item['currency']] = float(item['rate'])
-
-        resp['base'] = "EUR"
         resp['date'] = time['time']
         resp['rates'] = rates
         return resp
@@ -188,9 +167,8 @@ class Latest(View):
         return block['rate']
 
     def get(self, request):
-        self.get_data()
 
-        resp = self.parseData(symbols=request.GET.get('symbols'))
+        resp = self.parseData(base=request.GET.get('base'), symbols=request.GET.get('symbols'))
         # print("The price is: {} with type: {}".format(price, type(price)))
 
         # data = serializers.serialize('json', response)
@@ -201,11 +179,10 @@ class Latest(View):
 class getHist(View):
 
     def __init__(self):
-        self.data = None
+        self.data = get_hist_data()
 
     def get(self, request, year, month, day):
 
-        self.get_hist()
         soup = BeautifulSoup(self.data, 'html.parser')
         print("###########################################")
         print("soup hist: %s" % soup)
@@ -226,42 +203,90 @@ class getHist(View):
         return JsonResponse(resp)
 
 
-    def get_hist(self):
-        shelf = shelve.open('exchange_rates_hist')
-        if 'Last-Modified' in shelf:
-            print("################################################")
-            print("Data exist in cache, but checking for deprecation!")
-            headers = {'If-Modified-Since': shelf['Last-Modified']}
-            response = requests.get(url_hist, headers=headers)
-            try:
-                response.raise_for_status()
-            except:
-                print("Problem fetching data from network!!!")
+def get_data():
+    shelf = shelve.open('exchange_rates')
+    if 'Last-Modified' in shelf:
+        print("################################################")
+        print("Data exist in cache, but checking for deprecation!")
+        headers = {'If-Modified-Since': shelf['Last-Modified']}
+        response = requests.get(url_daily, headers=headers)
+        try:
+            response.raise_for_status()
+        except:
+            print("Problem fetching data from network!!!")
 
-            if response.text == '':
-                print("Data still good")
-                file = open("exchange_rates_hist.xml")
-                self.data = file.read()
-                file.close()
-            else:
-                print("Data is outdated")
-                self.data = response.text
-                shelf['Last-Modified'] = response.headers['Last-Modified']
-                file = open("exchange_rates_hist.xml", "w")
-                file.write(self.data)
-                file.close()
-        else:
-            print("################################################")
-            print("Fetching data from network")
-            response = requests.get(url_hist)
-            try:
-                response.raise_for_status()
-            except:
-                print("Problem fetching data from network!!!")
-
-            html = response.text
-            shelf['Last-Modified'] = response.headers['Last-Modified']
-            self.data = html
-            file = open("exchange_rates_hist.xml", "w")
-            file.write(html)
+        if response.text == '':
+            print("Data still good")
+            file = open("exchange_rates.xml")
+            data = file.read()
             file.close()
+        else:
+            print("Data is outdated")
+            data = response.text
+            shelf['Last-Modified'] = response.headers['Last-Modified']
+            file = open("exchange_rates.xml", "w")
+            file.write(data)
+            file.close()
+        return data
+
+    else:
+        print("################################################")
+        print("Fetching data from network")
+        response = requests.get(url_daily)
+        try:
+            response.raise_for_status()
+        except:
+            print("Problem fetching data from network!!!")
+
+        html = response.text
+        shelf['Last-Modified'] = response.headers['Last-Modified']
+        data = html
+        file = open("exchange_rates.xml", "w")
+        file.write(html)
+        file.close()
+        return data
+
+def get_hist_data():
+    shelf = shelve.open('exchange_rates_hist')
+    if 'Last-Modified' in shelf:
+        print("################################################")
+        print("Data exist in cache, but checking for deprecation!")
+        headers = {'If-Modified-Since': shelf['Last-Modified']}
+        response = requests.get(url_hist, headers=headers)
+        try:
+            response.raise_for_status()
+        except:
+            print("Problem fetching data from network!!!")
+
+        if response.text == '':
+            print("Data still good")
+            file = open("exchange_rates_hist.xml")
+            data = file.read()
+            file.close()
+        else:
+            print("Data is outdated")
+            data = response.text
+            shelf['Last-Modified'] = response.headers['Last-Modified']
+            file = open("exchange_rates_hist.xml", "w")
+            file.write(data)
+            file.close()
+        return data
+
+    else:
+        print("################################################")
+        print("Fetching data from network")
+        response = requests.get(url_hist)
+        try:
+            response.raise_for_status()
+        except:
+            print("Problem fetching data from network!!!")
+
+        html = response.text
+        shelf['Last-Modified'] = response.headers['Last-Modified']
+        data = html
+        file = open("exchange_rates_hist.xml", "w")
+        file.write(html)
+        file.close()
+        return data
+
+
--- exchange/static/exchange/js/bundle.js
+++ exchange/static/exchange/js/bundle.js
@@ -17,12 +17,12 @@ function process() {
     console.log('select_base:', base_value);
     console.log('select_target:', target_value);
     fetch(`http://192.168.0.171:8000/api/?amount=${amount}&base=${base_value}&target=${target_value}`)
-    .then(resp => resp.json())
-    .then(data => {
-        console.log(data);
-        input_target.value = data.response;
-    })
-    .catch(err => console.error(err));
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            input_target.value = data.response;
+        })
+        .catch(err => console.error(err));
 }
 
 select_target.addEventListener('change', validate);
@@ -31,7 +31,7 @@ function validate() {
     let select = event.target;
     event.stopPropagation();
     console.log('hello: ', select.selectedOptions[0].value);
-    if(input_base.value != '' && parseInt(input_base.value) != 0 && parseInt(input_base.value).toString() != "NaN") {
+    if (input_base.value != '' && parseInt(input_base.value) != 0 && parseInt(input_base.value).toString() != "NaN") {
         process();
     }
     else {
@@ -41,50 +41,71 @@ function validate() {
     }
 }
 
-function grap_latest() {
+function grab_latest() {
     fetch('http://192.168.0.171:8000/api/latest')
-    .then(resp => resp.json())
-    .then(data => {
-        console.log(data);
-        output.innerText = JSON.stringify(data, undefined, 4);
-        //rates.innerText = data.price;
-        output.classList.add("show");
-    })
-    .catch(err => console.error(err));
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            output.innerText = JSON.stringify(data, undefined, 4);
+            //rates.innerText = data.price;
+            output.classList.add("show");
+        })
+        .catch(err => console.error(err));
 }
 
 function grab_hist() {
     fetch('http://192.168.0.171:8000/api/2018-03-14')
-    .then(resp => resp.json())
-    .then(data => {
-        console.log(data);
-        output_hist.innerText = JSON.stringify(data, undefined, 4);
-        //rates.innerText = data.price;
-        output_hist.classList.add("show");
-    })
-    .catch(err => console.error(err));
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            output_hist.innerText = JSON.stringify(data, undefined, 4);
+            //rates.innerText = data.price;
+            output_hist.classList.add("show");
+        })
+        .catch(err => console.error(err));
 }
 
 function grab_base() {
-    fetch('http://192.168.0.171:8000/api/latest?symbols=USD,GBP')
-    .then(resp => resp.json())
-    .then(data => {
-        console.log(data);
-        output_base.innerText = JSON.stringify(data, undefined, 4);
-        //rates.innerText = data.price;
-        output_base.classList.add("show");
-    })
-    .catch(err => console.error(err));
+    fetch('http://192.168.0.171:8000/api/latest?base=USD&symbols=EUR,GBP,JPY')
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            output_base.innerText = JSON.stringify(data, undefined, 4);
+            //rates.innerText = data.price;
+            output_base.classList.add("show");
+        })
+        .catch(err => console.error(err));
 }
 
 function grab_symbols() {
     fetch('http://192.168.0.171:8000/api/latest?symbols=USD,GBP')
-    .then(resp => resp.json())
-    .then(data => {
-        console.log(data);
-        output_symbols.innerText = JSON.stringify(data, undefined, 4);
-        //rates.innerText = data.price;
-        output_symbols.classList.add("show");
-    })
-    .catch(err => console.error(err));
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            output_symbols.innerText = JSON.stringify(data, undefined, 4);
+            //rates.innerText = data.price;
+            output_symbols.classList.add("show");
+        })
+        .catch(err => console.error(err));
 }
+
+function copy(selector) {
+
+    let ele = document.querySelector(selector);
+    var range = document.createRange();
+    var selection = window.getSelection();
+    range.selectNodeContents(ele);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    try {
+        document.execCommand('copy');
+    }
+    catch (err) {
+        console.log(err);
+    }
+    selection.removeAllRanges();
+}
+
+document.addEventListener('copy', (e) => {
+    console.log(e.target.innerText);
+});
